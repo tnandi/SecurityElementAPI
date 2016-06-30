f5e309dde120de4cceb57373406997b4fae5a918
Tanmoy <Tanmoy@TANMOY-PROBOOK>
2016-07-01 02:41:50 +0530

    Add project files.

---
+++ SecurityElementAPI/Controllers/SecurityElementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SecurityElementAPI.Models;
+
+
+namespace SecurityElementAPI.Controllers
+{
+    public class SecurityElementController : ApiController
+    {
+        // POST: api/Products
+        [ResponseType(typeof(SecurityElementResponse))]
+        public IHttpActionResult PostProduct(SecurityElementRequest req)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            SecurityElementDBEntities db = new SecurityElementDBEntities();
+            SecurityElementResponse resp = new SecurityElementResponse()
+            {
+                Id = req.Id,
+                UID = req.UID,
+                CID = req.CID
+            };
+
+            if(db.SecurityMasters.Find(req.Id) == null)
+            {
+                resp.Ack = "FAIL";
+            }
+            else
+            {
+                resp.Ack = "SUCCESS";
+            }
+
+
+            return null;
+        }
+    }
+}
---
+++ SecurityElementAPI/Models/SecurityElementModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecurityElementAPI.Models
+{
+    public class SecurityElementRequest
+    {
+        public string MethodToCall { get; set; }
+        public int Id { get; set; }
+        public string UID { get; set; }
+        public string CID { get; set; }
+    }
+    public class SecurityElementResponse
+    {
+        public int Id { get; set; }
+        public string UID { get; set; }
+        public string CID { get; set; }
+        public string Ack { get; set; }
+    }
+}
